0282d54e021af7bfc342027f58c050fc4a0f57ae
Tarik Carey <renewed@gmail.com>
2024-12-14 17:25:44 +0000

    Postman Testing

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const tasks = require('./routes/tasks')
 
 
 //middleware
-
 app.use(express.json())
-
+app.use('/api/v1/tasks', tasks)
 
 //routes
 app.get('/hello', (req, res) => {
-    res.send('Task Manager App');
+  res.send('Task Manager App');
 })
 
 //app.get('/api/v1/tasks')                - get all the tasks
@@ -21,6 +20,6 @@ app.get('/hello', (req, res) => {
 
 
 
-app.use('/api/v1/tasks', tasks)
+
 const port = 3001;
 app.listen(port, console.log(`Server is listening on port ${port}....`));
--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,5 +1,30 @@
 const getAllTasks = (req, res) => {
-    res.send('all items')
+   res.send('get all tasks')
 }
 
-modu
+
+const createTask = (req, res) => {
+    res.send('create task')
+ }
+
+
+const getTask= (req, res) => {
+    res.send('get a single task') 
+ }
+
+ const updateTask = (req, res) => {
+    res.send('update task') 
+ }
+
+ const deleteTask = (req, res) => {
+    res.send('delete task')
+ }
+
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
