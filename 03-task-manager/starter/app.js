a46839db5b7cb2f1d63353b93d4fbcc52b017366
Tarik Carey <renewed@gmail.com>
2024-12-14 16:39:13 +0000

    today

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -13,6 +13,14 @@ app.get('/hello', (req, res) => {
     res.send('Task Manager App');
 })
 
+//app.get('/api/v1/tasks')                - get all the tasks
+//app.post('/api/v1/tasks')               - create a new tasks
+//app.get('/api/v1/tasks/:id')            - get a single task
+//app.patch('/api/v1/tasks/:id')                - get all the tasks
+//app.delete('/api/v1/tasks/:id')                - get all the tasks
+
+
+
 app.use('/api/v1/tasks', tasks)
 const port = 3001;
 app.listen(port, console.log(`Server is listening on port ${port}....`));
